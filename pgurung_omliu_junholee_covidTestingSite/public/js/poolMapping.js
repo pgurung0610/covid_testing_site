4a888a3ea4e62e27ff7328c312abe58da6d47fe9
Oliver Liu <oliver.liu@stonybrook.edu>
2020-11-22 20:05:12 -0500

    Front End Templates Lab Home, Pool Mapping, Well Testing

---
+++ pgurung_omliu_junholee_covidTestingSite/public/js/poolMapping.js
@@ -0,0 +1,45 @@
+
+let inputs = 0
+
+
+function addRow() {
+    inputs++
+
+    let newLI = document.createElement("li")
+    
+    let newInput = document.createElement("input")
+    newInput.setAttribute("type", "text")
+    newInput.setAttribute("name", `b${inputs}`)
+
+    let newBtn = document.createElement("button")
+    let deleteTextNode = document.createTextNode("Delete")
+    newBtn.appendChild(deleteTextNode)
+    newBtn.addEventListener('click', (evt) => {
+        evt.preventDefault()
+        removeElement(newLI)
+    })
+
+    
+    newLI.appendChild(newInput)
+    newLI.appendChild(newBtn)
+
+    var lst = document.getElementById("barcodesList")
+    lst.appendChild(newLI)
+//     var newPara = document.createElement("p");
+// var content = document.createTextNode("This is a new paragraph.");
+// newPara.appendChild(content);
+// var divElem = document.getElementById("theDiv");
+// divElem.appendChild(newPara);
+}
+
+for(i = 0; i < 2; i++)
+    addRow()
+
+function removeElement(element) {
+    element.parentNode.removeChild(element)
+}
+
+document.getElementById("addRow").addEventListener('click', (evt)=>{
+    evt.preventDefault()
+    addRow()
+})
